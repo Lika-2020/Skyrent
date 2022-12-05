6a1f6d0ce96d8d5f560a60f7809a88380dcecd8a
Анжелика <lika-2020@yandex.ru>
2022-12-05 21:46:28 +0300

    the end progect

--- detailed-information.js
+++ detailed-information.js
@@ -1,3 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+console.log(id);
+
 const more = document.querySelector('.more');
 const moreText = document.querySelector('.more__text');
 
@@ -5,11 +9,7 @@ const button = document.querySelector('.detailed-information__button');
 const contacts = document.querySelector('.contacts');
 
 const back = document.querySelector('.logo__arrow');
-
-more.addEventListener('click', () => {
-    moreText.classList.remove('more__text');
-    
-});
+const contactsInfo = document.querySelector('.info');
 
 button.addEventListener('click', () => {
     contacts.classList.remove('contacts');
@@ -19,3 +19,114 @@ button.addEventListener('click', () => {
 back.addEventListener('click', () => {
     window.location.href = 'index.html';
 });
+
+const section = document.querySelector('.product-content');
+const sectionDetailedInformation = document.querySelector('.detailed-information');
+
+const OFFER = `http://45.8.249.186/offers/${id}`;
+
+request({
+
+    url: OFFER,
+
+    onSuccess: (data) => {
+        section.innerHTML = '';
+
+        const productContentDescription = document.createElement('div');
+        productContentDescription.classList.add('product-content__description');
+
+        const h3 = document.createElement('h3');
+        h3.classList.add('product-content__heading');
+        h3.textContent = (data.country + ' ' + '→' + ' ' + data.city);
+
+        const span = document.createElement('span');
+        span.classList.add('product-content__price');
+        span.textContent = ('$' + ' ' + data.price + ' ' + '/' + ' ' + 'month');
+
+
+        const p = document.createElement('p');
+        p.classList.add('product-content__text');
+        p.textContent = data.description;
+        console.log(data.description);
+
+        const productContentImg = document.createElement('div');
+        productContentImg.classList.add('product-content__img');
+
+        const img = document.createElement('img');
+        img.classList.add('img-content');
+        img.src = data.picture_url;
+
+        const detailedInformationContent = document.createElement('div');
+        detailedInformationContent.classList.add('detailed-information__content');
+
+        const h4 = document.createElement('h4');
+        h4.classList.add('detailed-information__title');
+        h4.textContent = 'Что есть внутри?';
+
+        const ol = document.createElement('ol');
+        ol.classList.add('hidden');
+
+        const li = document.createElement('li');
+        li.textContent = data.features_on;
+
+        const contact = document.createElement('div');
+        contact.classList.add('contact');
+
+        const contactName = document.createElement('p');
+        contactName.classList.add('contact__name');
+        contactName.textContent = 'Имя хоста';
+
+        const contactText = document.createElement('span');
+        contactText.classList.add('.contact__text');
+        contactText.textContent = data.host_name;
+
+        const contactNamePhone = document.createElement('p');
+        contactNamePhone.classList.add('contact__name');
+        contactNamePhone.textContent = 'Телефон';
+
+        const contactTextPhone = document.createElement('span');
+        contactTextPhone.classList.add('contact__text');
+        contactTextPhone.textContent = data.host_phone;
+
+        const contactNameAddress = document.createElement('p');
+        contactNameAddress.classList.add('contact__name');
+        contactNameAddress.textContent = 'Адрес';
+
+        const contactTextAddress = document.createElement('span');
+        contactTextAddress.classList.add('contact__text');
+        contactTextAddress.textContent = data.host_location;
+
+
+        section.appendChild(productContentDescription);
+        productContentDescription.appendChild(h3);
+        productContentDescription.appendChild(span);
+        productContentDescription.appendChild(p);
+        section.appendChild(productContentImg);
+        productContentImg.appendChild(img);
+        sectionDetailedInformation.appendChild(detailedInformationContent);
+        detailedInformationContent.appendChild(h4);
+        detailedInformationContent.appendChild(ol);
+        ol.appendChild(li);
+        contactsInfo.appendChild(contact);
+        contact.appendChild(contactName);
+        contact.appendChild(contactText);
+        contact.appendChild(contactNamePhone);
+        contact.appendChild(contactTextPhone);
+        contact.appendChild(contactNameAddress);
+        contact.appendChild(contactTextAddress);
+
+
+    }
+})
+
+
+
+/*
+
+/*<div class="contact">
+    <p class="contact__name">Имя хоста</p><span class="contact__text">Eustolia A. Daniel</span>
+
+    <p class="contact__name">Телефон</p><span class="contact__text">267-936-7078</span>
+
+    <p class="contact__name">Адрес</p><span class="contact__text">2310 Marigold Lane FL 33131</span>
+</div>*/
--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const container = document.querySelector('.container');
 const topContent = document.querySelector('.top-content');
 const button = document.querySelector('.top-content__button');
 const containerFilter = document.querySelector('.filter');
+const select = document.querySelector('.select__dropdown');
+const inputIn = document.querySelectorAll('.price__input');
+const btn = document.querySelector('.filter__button');
+
+
+
 
 button.addEventListener('click', () => {
 
@@ -10,7 +16,7 @@ button.addEventListener('click', () => {
 
 })
 
-document.querySelectorAll('.price__input').forEach(
+inputIn.forEach(
     item => item.addEventListener('keydown', function (event) {
 
         let arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace', 'Delete'];
@@ -24,53 +30,182 @@ document.querySelectorAll('.price__input').forEach(
 );
 
 
-    const section = document.querySelector('.product-content');
+const section = document.querySelector('.product-content');
+
+const OFFERS = 'http://45.8.249.186/offers/';
+
+request({
+
+    url: OFFERS,
+
+    onSuccess: (data) => {
+        render(data);
+        console.log(data);
+    }
+})
+
+function render(item) {
+    item.forEach(element => {
+        const productContentImg = document.createElement('div');
+        productContentImg.classList.add('product-content__img');
+
+        const a = document.createElement('a');
+        a.href = 'detailed-information.html?id=' + element.id;
+
+        const img = document.createElement('img');
+        img.classList.add('picture');
+        img.src = element.picture_url;
+
+        const productContentDescription = document.createElement('div');
+        productContentDescription.classList.add('product-content__description');
+
+
+        const h3 = document.createElement('h3');
+        h3.classList.add('product-content__heading');
+        h3.textContent = (element.country + ' ' + '→' + ' ' + element.city);
+
+        const p = document.createElement('p');
+        p.classList.add('product-content__text');
+        p.textContent = element.description;
+
+        const span = document.createElement('span');
+        span.classList.add('product-content__price');
+        span.textContent = ('$' + ' ' + element.price + ' ' + '/' + ' ' + 'month');
+
+
+        section.appendChild(productContentImg);
+        productContentImg.appendChild(a);
+        a.appendChild(img);
+        section.appendChild(productContentDescription);
+        productContentDescription.appendChild(h3);
+        productContentDescription.appendChild(p);
+        productContentDescription.appendChild(span);
+
+    });
+}
+
+
+
+request({
+
+    url: OFFERS,
+
+    onSuccess: (data) => {
+        renderSelect(data);
+    }
+})
+
+function renderSelect(data) {
+    data.forEach(element => {
+        const option = document.createElement('option');
+        option.textContent = (element.country + ' ' + '→' + ' ' + element.city);
+        select.appendChild(option);
+    });
+}
+
+const picture = document.querySelector('.picture');
+
+select.addEventListener('change', () => {
+    select.classList.add('btn-color');
+})
+
+let selectValue = '';
+btn.addEventListener('click', () => {
+    btn.classList.add('btn-color');
+    selectValue = select.value;
+
 
     request({
-        url: 'https://skypro-raids-01.onrender.com/offers/',
-
+        url: OFFERS,
         onSuccess: (data) => {
 
+            filterSearch(data);
+
+            /* const filtered = data.filter(data => data.country + ' ' + '→' + ' ' + data.city === value);
+             console.log(filtered);*/
+
+        }
+    });
+
+});
+
+function filterSearch(data) {
+    const rgx = new RegExp(selectValue);
+    let filtered = data.filter(card => {
+        if (rgx.test(card.country + ' ' + '→' + ' ' + card.city)) {
+            return true;
+        } else {
+            return false;
         }
     })
 
-    function render(data) {
-        data.forEach(element => {
-            const productContentImg = document.createElement('div');
-            productContentImg.classList.add('product-content__img');
+    section.innerHTML = '';
 
-            const a = document.createElement('a');
-            a.href = 'detailed-information.html';
+    filtered.forEach(element => {
 
-            const img = document.createElement('img');
-            img.src = element.picture_url;
-            img.style.height = '354px';
-            img.style.width = '330px';
+        const productContentImg = document.createElement('div');
+        productContentImg.classList.add('product-content__img');
 
-            const productContentDescription = document.createElement('div');
-            productContentDescription.classList.add('product-content__description');
+        const a = document.createElement('a');
+        a.href = 'detailed-information.html' + element.id;
 
-            const productContentName = document.createElement('a');
-            productContentName.classList.add('product-content__name');
-            productContentName.href = 'detailed-information.html';
+        const img = document.createElement('img');
+        img.classList.add('picture');
+        img.src = element.picture_url;
 
-            const h3 = document.createElement('h3');
-            h3.classList.add('product-content__heading');
-            h3.textContent = element.country / element.city;
-
-            const p = document.createComment('p');
-            p.classList.add('product-content__text');
-            p.textContent = 'Привет';
+        const productContentDescription = document.createElement('div');
+        productContentDescription.classList.add('product-content__description');
 
 
+        const h3 = document.createElement('h3');
+        h3.classList.add('product-content__heading');
+        h3.textContent = (element.country + ' ' + '→' + ' ' + element.city);
+
+        const p = document.createElement('p');
+        p.classList.add('product-content__text');
+        p.textContent = element.description;
+
+        const span = document.createElement('span');
+        span.classList.add('product-content__price');
+        span.textContent = ('$' + ' ' + element.price + ' ' + '/' + ' ' + 'month');
 
 
-            section.appendChild(productContentImg);
-            productContentImg.appendChild(a);
-            a.appendChild(img);
-            section.appendChild(productContentDescription);
-            productContentDescription.appendChild(productContentName);
-            productContentName.appendChild(h3);
+        section.appendChild(productContentImg);
+        productContentImg.appendChild(a);
+        a.appendChild(img);
+        section.appendChild(productContentDescription);
+        productContentDescription.appendChild(h3);
+        productContentDescription.appendChild(p);
+        productContentDescription.appendChild(span);
+
+    });
+
+    console.log(filtered);
+
+}
+
+
+
+
+/*function renderInput(data) {
+    inputIn.forEach(input => {
+        input.addEventListener("input", () => {
+            let minVal = inputIn[0].value;
+            let maxVal = inputIn[1].value;
+            console.log(minVal, maxVal);
+
+            if (maxVal - minVal < data.price) {
+                minVal = maxVal - data.price;
+
+            } else {
+                maxVal = minVal - data.price;
+            }
 
         })
-    }
+    });
+
+}*/
+//renderInput(data);
+
+
+
